48a1b95db4d2ae30ecbdb2e2e0e4d8a72dbf20d4
BaptisteDUB <74829941+BaptisteDUB@users.noreply.github.com>
2022-03-14 16:41:21 +0100

    Update run_node_container.sh

--- cicd/run_node_container.sh
+++ cicd/run_node_container.sh
@@ -2,4 +2,4 @@
 docker run --rm --name back_api \
 --network lms_network \
 -v $(pwd)/api:/usr/app/api -v /usr/app/api/node_modules \
--it back_api /bin/bash ./bin/start.sh
+back_api /bin/bash ./bin/start.sh
